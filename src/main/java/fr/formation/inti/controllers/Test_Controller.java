89b190a25683949298ef1eb276a9614627a47cec
IN-ST-013 <IN-ST-013@IN-ST-013.home>
2019-04-16 17:44:41 +0200

    test

--- src/main/java/fr/formation/inti/controllers/Test_Controller.java
+++ src/main/java/fr/formation/inti/controllers/Test_Controller.java
@@ -3,7 +3,11 @@ package fr.formation.inti.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+/**
+ * 
+ * @author IN-ST-013
+ *
+ */
 @Controller
 public class Test_Controller {
 	
